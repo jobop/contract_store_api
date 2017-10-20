a9dc0b42f7c31dada37db98638bf0cd3d4caad23
unknown <zhengw@cmfdev.cmfchina.com>
2017-10-20 16:39:18 +0800

    增加owner转换

--- contracts/Generatable.sol
+++ contracts/Generatable.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.4.13;
 // Contracts that can be converted into other sourced of value
 // should implement this.
 contract Generatable{
-    function generate() public returns(address);
+    function generate(address contractOwner) public returns(address);
 }
